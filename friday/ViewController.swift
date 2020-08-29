a96c5bf8ba06810bbab75a810e7203443b09db25
1106137105 <1106137105@nkust.edu.tw>
2020-08-29 16:32:10 +0800

    Initial Commit

---
+++ friday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  friday
+//
+//  Created by 溫家豪 on 2020/8/29.
+//  Copyright © 2020 溫家豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
